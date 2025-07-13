7871e5ff0321b85762e2c1fd5a29b164df0bbf62
mellojp <jpedrom941@gmail.com>
2025-07-13 14:03:56 -0300

    Update qm_algorithm.c

--- qm_algorithm.c
+++ qm_algorithm.c
@@ -5,6 +5,14 @@
 
 typedef char* string;
 
+//função auxiliar para testes
+void imprimirMintermos(string* mintermos, uint32_t quantidade) {
+    printf("Mintermos armazenados:\n");
+    for (uint32_t i = 0; i < quantidade; i++) {
+        printf("%s\n", mintermos[i]);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     if(argc != 2) {
@@ -19,30 +27,50 @@ int main(int argc, char const *argv[])
     }
 
     char buffer[256];
-    uint32_t numEntradas, numSaidas;
+    uint32_t numEntradas, numSaidas, i = 0;
+    string* mintermos = NULL;
 
     while(fgets(buffer, sizeof(buffer), input)){ //lê cada linhda do .pla
 
         buffer[strcspn(buffer,"\n")] = 0; //remove o /n
         if(buffer[0]=='\0') continue; //ignora linhas vazias
 
-        if(buffer[0] == '.') {
+        if(buffer[0] == '.') { //adicionar .type e .ilb / .ob (se precisar)
             if (buffer[1] == 'i') sscanf(buffer,".i %u",&numEntradas);
             if (buffer[1] == 'o') sscanf(buffer,".o %u",&numSaidas);
             if (buffer[1] == 'e') break;
         }
         else {
-            string entrada = malloc(numEntradas*sizeof(char));
-            string saida = malloc(numSaidas*sizeof(char)); 
-            sscanf(buffer, "%s %s", entrada, saida);
+                string entrada = malloc((numEntradas + 1)*sizeof(char));
+                string saida = malloc((numSaidas + 1)*sizeof(char)); 
+                sscanf(buffer, "%s %s", entrada, saida);
+                
+                if(strcmp(saida,"1")==0){
+                    string* temp = realloc(mintermos, sizeof(string) * (i + 1));
+                    if (temp != NULL) {
+                        mintermos = temp;
+                        mintermos[i] = entrada; 
+                        i++;
+                    } else {
+                        free(entrada); 
+                        free(saida);    
+                        fprintf(stderr, "Erro ao alocar memória.\n");
+                        return 1;
+                    }
+                } else free(entrada); //libera sempre que a saida for 0 (a entrada não vai ser utilizada)
 
-            if(strcmp(saida,"0")==1){ 
-                //ignora os mintermos que são 0
-                //modificar para adicionar as entradas em um vetor
-                printf("%s %s\n",entrada, saida);
+                free(saida); //libera anyways
             }
-        }
-    }    
+        }    
 
+        imprimirMintermos(mintermos, i); //teste para vez se tá armazenando legal
+
+        //LIBERAR MEMORIA ALOCADA
+        for (uint32_t j = 0; j < i; j++) {
+             free(mintermos[j]);
+        }
+        free(mintermos);
+
+        fclose(input);
     return 0;
 }
